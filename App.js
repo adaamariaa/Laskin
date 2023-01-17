d378b3d588ca39e71fbcdcafde30707427ea36d6
adaamariaa <ada.merilainen@gmail.com>
2023-01-17 20:58:37 +0200

    Bug Fixed

--- App.js
+++ App.js
@@ -1,20 +1,84 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Button, TextInput, Text } from 'react-native';
 
 export default function App() {
+  const [firstNumber, setfirstNumber] = React.useState(0);
+  const [secondNumber, setsecondNumber] = React.useState(0);
+  const [answer, setAnswer] = React.useState(firstNumber + secondNumber);
+
+  const buttonPlus = () => {
+    const first=parseInt(firstNumber);
+    const second=parseInt(secondNumber);
+    const sum=first + second;
+    setAnswer(sum);
+  }
+
+  const buttonMinus = () => {
+    const sum=firstNumber - secondNumber
+    setAnswer(sum);
+  }
+
+  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <View>
+          <Text>
+            {'Result: '+ answer}
+          </Text>
+        </View>
+        <View>
+        
+        <TextInput
+          style={{width: 200, borderColor: 'black', borderWidth: 1,  }}
+          keyboardType={'numeric'}
+          value={firstNumber}
+          onChangeText={number => setfirstNumber(number)}
+        />
+        <TextInput
+          style={{width: 200, borderColor: 'black', borderWidth: 1 }}
+          keyboardType={'numeric'}
+          value={secondNumber}
+          onChangeText={number => setsecondNumber(number)}
+        />
+        </View>
+        <View>
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              <Button color='pink' onPress={buttonPlus} title="+"/>
+              </View>
+              <View>
+              <Button color='pink' onPress={buttonMinus} title="-"/>
+              </View>
+          <StatusBar style="auto" />
+          </View>
+        </View>
+      </View>
+
   );
-}
+  }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    marginTop: 200,
+    justifyContent: 'center', 
     alignItems: 'center',
-    justifyContent: 'center',
+    alignContent: 'center',
+    backgroundColor: '#fff',
+    margin: 10,
   },
+
+  buttonContainer: {
+    marginTop: 10,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+  },
+
+  button: {
+    marginRight: 10,
+  },
+
+
 });
